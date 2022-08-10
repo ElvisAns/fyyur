170d1249429e1b0d6dc3ef5864b9e386e6a6628c
ElvisAns <ansimapersic@gmail.com>
2022-08-10 12:26:39 +0200

    Implement route to search venues

--- projects/01_fyyur/app.py
+++ projects/01_fyyur/app.py
@@ -186,14 +186,14 @@ def search_venues():
   # TODO: implement search on venues with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  search = request.form.get("search_term")
+  result = Venue.query.filter(Venue.name.ilike(f'%{search}%') | Venue.name.ilike(f'{search}%')| Venue.name.ilike(f'%{search}')).all()
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(result),
+    "data": result
   }
+  
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
