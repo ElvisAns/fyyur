b7885737adb463f56e4fcaadbf387f61f50278aa
ElvisAns <ansimapersic@gmail.com>
2022-08-10 02:18:47 +0200

    Implement Artist search

--- projects/01_fyyur/app.py
+++ projects/01_fyyur/app.py
@@ -318,13 +318,12 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  search = request.form.get("search_term")
+  result = Artist.query.filter(Artist.name.ilike(f'%{search}%') | Artist.name.ilike(f'{search}%')| Artist.name.ilike(f'%{search}')).all()
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(result),
+    "data": result
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
